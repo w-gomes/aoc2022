f284dbef39b93e009855671ce591817e4d901403
W Gomes <w.gomes5521250@gmail.com>
2022-12-02 21:27:19 -0300

    fmt

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,13 +1,13 @@
+use itertools::sorted;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use itertools::sorted;
 
 fn main() -> anyhow::Result<()> {
     let file = File::open("inputs/day1.txt")?;
     let file = BufReader::new(file);
     let mut vec = Vec::new();
     let mut acc = 0;
-    
+
     for line in file.lines() {
         if let Ok(num) = line {
             match num.parse::<u32>() {
--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,6 +1,10 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() -> anyhow::Result<()> {
-    let file = std::fs::read_to_string("inputs/day1.txt")?;
-    let timer = std::time::Instant::now();
+    let file = std::fs::read_to_string("inputs/day?.txt")?;
+    let file = File::open("inputs/day?.txt")?;
+    let file = BufReader::new(file);
 
     Ok(())
 }
